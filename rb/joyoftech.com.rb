cd0e89f86dfcc5d2acf95a29540d1391786c8ebb
Max Howell <max@methylblue.com>
2009-08-17 17:29:48 +0100

    Joy of tech block hot linking to their images
    So I won't try to bypass that, it's their wish.

--- rb/joyoftech.com.rb
+++
@@ -1,9 +0,0 @@
-require 'threepanes'
-
-puts "Joy of Tech"
-puts "Geek"
-
-get_rss 'http://www.joyoftech.com/joyoftech/jotblog/index.xml' do |item|
-  n=File.basename item.link, '.html'
-  comic "joyoftech/joyimages/#{n}.jpg", item.pubDate, item.title
-end
