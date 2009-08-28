942eef026d4ab8af3911c24e6739e9704d6b595a
Max Howell <max@methylblue.com>
2009-08-28 19:23:38 +0100

    FIX Penny Arcade

--- rb/penny-arcade.com.rb
+++ rb/penny-arcade.com.rb
@@ -4,7 +4,7 @@ puts "Penny Arcade"
 puts "Gamer"
 
 get_rss 'http://feeds.penny-arcade.com/pa-mainsite' do |item|
-  next unless /^New Comic :/ =~ item.description
+  next unless /^New Comic\s*:/ =~ item.description
   year=item.pubDate.year
   month=item.pubDate.month.to_s.rjust 2, '0'
   day=item.pubDate.mday.to_s.rjust 2, '0'
