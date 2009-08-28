258e63e84c6d4541eaf07a90b823ce8fe409d23a
Max Howell <max@methylblue.com>
2009-08-28 19:50:08 +0100

    Fix RoosterTeeth.com

--- rb/roosterteeth.com.rb
+++ rb/roosterteeth.com.rb
@@ -5,6 +5,6 @@ puts "Rooster Teeth"
 puts "Gamer"
 
 get_rss 'http://roosterteeth.com/_rss/news.rss' do |item|
-  item.description =~ /src=(.*?)t.jpg/
+  item.description =~ /src="(.*?)t.jpg"/
   comic "#{$1}.jpg", item.pubDate, item.title
 end
